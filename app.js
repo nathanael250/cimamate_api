32d93460df148f101864a1e1669f8f24436feca8
nathanael250 <nathanaelniyogushimwa@gmail.com>
2025-05-21 11:25:23 +0200

    changing mongoose

--- app.js
+++ app.js
@@ -14,11 +14,10 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => console.log("MongoDB connected"))
-    .catch(err => console.error(err));
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("✅ MongoDB connected"))
+    .catch((err) => console.error("❌ MongoDB connection error:", err));
+
 
 // Routes
 app.use("/api/auth", authRoutes);
